cc69fa197fc29b06344184020d406998c148b26c
AungKhantSoe21 <78907333+AungKhantSoe21@users.noreply.github.com>
2022-12-15 10:28:55 +0630

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
       title: appName,
       theme: getTheme(),
       home: const HomeScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Dashboard"),
+      ),
       body: Container(),
     );
   }
